c1644b1c1af9cae51f4691ffa4a0140d0493fc76
emmyleaf <60900810+emmyleaf@users.noreply.github.com>
2020-11-25 21:52:55 +0000

    Deploying to main from  @ 8b9ec3334ac8a0d68e4a4dc76d02f2604dcd2db3 🚀

--- _next/static/cAf23NYnwYeXlAEb02ND2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-2987c40b06114dbf8ad7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6632e0a19024d1123051.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
